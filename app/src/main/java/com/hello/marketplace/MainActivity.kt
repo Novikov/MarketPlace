76965bad3936cdcc81da1445d8d77f976ef17a15
Igor Novikov <igor.novikov@umbrellait.com>
2022-07-09 17:09:01 +0300

    player module has been added + koin dependency

--- app/src/main/java/com/hello/marketplace/MainActivity.kt
+++ app/src/main/java/com/hello/marketplace/MainActivity.kt
@@ -2,10 +2,12 @@ package com.hello.marketplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hello.player.Player
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Player().runPlayer()
     }
 }
---
+++ player/src/main/java/com/hello/player/Player.kt
@@ -0,0 +1,9 @@
+package com.hello.player
+
+import android.util.Log
+
+class Player {
+    fun runPlayer() {
+        Log.i("ASDASDASDASDASDADAS", "runPlayer:a")
+    }
+}
